cecd21868eb1292f3603ec02508869dffda353fd
satish_r1 <satish_r1@verifone.com>
2020-08-09 18:31:04 +0530

    Revert "Added Queries for last 9 datasets i.e except Plu,Department and Category."
    This reverts commit 18944cc0ef708ce920a3a1afba4380a926eed4f3.

--- src/main/java/com/verifone/analytics/spark/transset/SparkTranssetAnalyser.java
+++ src/main/java/com/verifone/analytics/spark/transset/SparkTranssetAnalyser.java
@@ -3,8 +3,6 @@ package com.verifone.analytics.spark.transset;
 import static org.apache.spark.sql.functions.col;
 import static org.apache.spark.sql.functions.explode;
 import static org.apache.spark.sql.functions.substring;
-import static org.apache.spark.sql.functions.to_date;
-import static org.apache.spark.sql.functions.to_utc_timestamp;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -114,105 +112,47 @@ public class SparkTranssetAnalyser {
 	}
 
 	private void salesByFuelProduct(SparkSession sparkSession, Dataset<Row> transactionDS) {
-		Dataset<Row> trLines = transactionDS.select(to_date(to_utc_timestamp(col("trans.trHeader.date"),"YYYY-MM-DD")).as("trDate"),col("trans.type"), col("trans.trValue"),
-				col("trans.trHeader.trTickNum.trSeq"),explode(col("trans.trLines.trLine")).as("trLines")).filter(col("trLines.type").equalTo("postFuel"));
-		trLines.createOrReplaceTempView("itemlines");
-		trLines.show();
-		trLines.printSchema();
-		Dataset<Row> salesByFuelProduct  = sparkSession
-				.sql("SELECT trDate as Date,trLines.trlFuel.fuelProd.value as FuelProductDesc,sum(abs(trLines.trlLineTot)) as Amount,sum(abs(trLines.trlFuel.fuelVolume)) as Volume FROM itemlines  GROUP BY Date,FuelProductDesc ORDER BY Date desc").filter(col("Amount").gt(new Double("0.0")));
-	   salesByFuelProduct.show();
-	   writeToResultSetDB(salesByFuelProduct, "salesByFuelProduct");
+		// TODO Auto-generated method stub
 		
 	}
 
 	private void customerWaitTimeByCashier(SparkSession sparkSession, Dataset<Row> transactionDS) {
-		Dataset<Row> transactions = transactionDS.select(to_date(to_utc_timestamp(col("trans.trHeader.date"),"YYYY-MM-DD")).as("Date"),col("trans.trHeader.cashier.value").as("CashierName"),
-				col("trans.trHeader.duration").as("WaitTime"));
-		transactions.createOrReplaceTempView("transactionsByCashier");
-		transactions.show();
-		Dataset<Row> averageWaitTimeByCashier = sparkSession.sql("select Date,CashierName,avg(WaitTime) as AvgWaitTime from transactionsByCashier GROUP BY Date,CashierName Order By CashierName desc");
-		averageWaitTimeByCashier.show();
-		writeToResultSetDB(averageWaitTimeByCashier, "averageWaitTimeByCashier");
+		// TODO Auto-generated method stub
+		
 	}
 
 	private void dailySalesCount(SparkSession sparkSession, Dataset<Row> transactionDS) {
-		Dataset<Row> transactions = transactionDS.select(to_date(to_utc_timestamp(col("trans.trHeader.date"),"YYYY-MM-DD")).as("Date"),col("trans.type"));
-		transactions.createOrReplaceTempView("transactionCounts");
-		transactions.show();
-		Dataset<Row> dailySalesCount  = sparkSession.sql("select Date,count(type) as TxnCount from transactionCounts GROUP BY Date order by Date desc");
-		dailySalesCount .show();
-		writeToResultSetDB(dailySalesCount ,"dailySalesCount");
+		// TODO Auto-generated method stub
 		
 	}
 
 	private void averageTransactionAmount(SparkSession sparkSession, Dataset<Row> transactionDS) {
-		Dataset<Row> transactions = transactionDS.select(to_date(to_utc_timestamp(col("trans.trHeader.date"),"YYYY-MM-DD")).as("Date"),col("trans.type"),
-				col("trans.trValue.trTotWTax"));
-		transactions.createOrReplaceTempView("transactionTotals");
-		transactions.show();
-		Dataset<Row> averageTransactionAmount = sparkSession.sql("select Date,avg(trTotWTax) as AvgAmount from transactionTotals GROUP BY Date order by Date desc");
-		averageTransactionAmount.show();
-		writeToResultSetDB(averageTransactionAmount,"averageTransactionAmount");
+		// TODO Auto-generated method stub
 		
 	}
 
 	private void salesByMOP(SparkSession sparkSession, Dataset<Row> transactionDS) {
-		Dataset<Row> trPayLines = transactionDS.select(to_date(to_utc_timestamp(col("trans.trHeader.date"),"YYYY-MM-DD")).as("trDate"),col("trans.type"),
-				(explode(col("trans.trPaylines.trPayline")).as("trPaylines"))).filter(col("trPaylines.trpPayCode.value").notEqual("Change"));
-		trPayLines.createOrReplaceTempView("paymentlines");
-		trPayLines.show();
-		trPayLines.printSchema();
-		Dataset<Row> salesByMOP = sparkSession
-				.sql("SELECT trDate as Date, trPayLines.trpPayCode.value as MopName, count(trPaylines) as Count FROM paymentlines Group By Date,MopName order by Date desc ");
-		salesByMOP.show();
-		writeToResultSetDB(salesByMOP, "salesByMOP");
+		// TODO Auto-generated method stub
 		
 	}
 
 	private void salesByCardType(SparkSession sparkSession, Dataset<Row> transactionDS) {
-		Dataset<Row> trPayLines = transactionDS.select(to_date(to_utc_timestamp(col("trans.trHeader.date"),"YYYY-MM-DD")).as("trDate"),col("trans.type"),
-				(explode(col("trans.trPaylines.trPayline")).as("trPaylines"))).filter(col("trPaylines.trpPayCode.value").notEqual("Change"));
-		trPayLines.createOrReplaceTempView("paymentlines");
-		Dataset<Row> salesByCardType = sparkSession
-				.sql("SELECT trDate as Date,trPayLines.trpCardInfo.trpcCCName.value as CardType,count(trPaylines) as Count FROM paymentlines GROUP By Date,CardType Order by Date desc");
-		writeToResultSetDB(salesByCardType, "salesByCardType");
+		// TODO Auto-generated method stub
 		
 	}
 
 	private void salesByEntryMethod(SparkSession sparkSession, Dataset<Row> transactionDS) {
-		Dataset<Row> trPayLines = transactionDS.select(to_date(to_utc_timestamp(col("trans.trHeader.date"),"YYYY-MM-DD")).as("trDate"),col("trans.type"),
-				(explode(col("trans.trPaylines.trPayline")).as("trPaylines"))).filter(col("trPaylines.trpPayCode.value").notEqual("Change"));
-		trPayLines.createOrReplaceTempView("paymentlines");
-		Dataset<Row> salesByEntryMethod = sparkSession
-				.sql("SELECT trDate as Date,trPayLines.trpCardInfo.trpcEntryMeth as EntryMethod,count(trPaylines) as Count FROM paymentlines GROUP By Date,EntryMethod Order by Date desc");
-		salesByEntryMethod.show();
-		writeToResultSetDB(salesByEntryMethod, "salesByEntryMethod");
+		// TODO Auto-generated method stub
+		
 	}
 
 	private void discountsByLoyaltyProgram(SparkSession sparkSession, Dataset<Row> transactionDS) {
-		Dataset<Row> trLines = transactionDS.select(to_date(to_utc_timestamp(col("trans.trHeader.date"),"YYYY-MM-DD")).as("trDate"),col("trans.type"), col("trans.trValue"),
-				explode(col("trans.trLines.trLine")).as("trLines"),col("trans.trHeader.trTickNum.trSeq"));
-		trLines.createOrReplaceTempView("itemlines");
-		trLines.show();
-		trLines.printSchema();
-		Dataset<Row> trLoyLines = trLines.select(col("trDate"),explode(col("trLines.trloPPGDisc")).as("trLoyLines"));
-		trLoyLines.createOrReplaceTempView("loyaltyItemlines");
-		trLoyLines.show();
-		trLoyLines.printSchema();		
-		Dataset<Row> discountsByLoyaltyProgram = sparkSession
-				.sql("SELECT trDate as Date,trLoyLines.programID as LoyaltyProgram,sum(trLoyLines.PPGTotDisc) as DiscAmount FROM loyaltyItemlines Group By Date,LoyaltyProgram Order By Date desc");
-		writeToResultSetDB(discountsByLoyaltyProgram, "discountsByLoyaltyProgram");
+		// TODO Auto-generated method stub
 		
 	}
 
 	private void recurringCustomerCount(SparkSession sparkSession, Dataset<Row> transactionDS) {
-		Dataset<Row> trPayLines = transactionDS.select(col("trans.type"),
-				(explode(col("trans.trPaylines.trPayline")).as("trPaylines"))).filter(col("trPaylines.trpPayCode.value").notEqual("Change"));
-		trPayLines.createOrReplaceTempView("paymentlines");
-		Dataset<Row> recurringCustomerCount = (Dataset<Row>) sparkSession
-				.sql("SELECT trPayLines.trpCardInfo.trpcAccount as CustCardNumber,count(trPaylines) as Count FROM paymentlines GROUP By trPayLines.trpCardInfo.trpcAccount  Order by Count desc").filter(col("CustCardNumber").isNotNull()).limit(10);
-		writeToResultSetDB(recurringCustomerCount, "recurringCustomerCount");
+	// TODO Auto-generated method stub
 	
 	}
 
