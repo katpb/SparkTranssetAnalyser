95cc73602b57080193077f3cd696d60c044645f6
Kathir Prabhakaran Shanmugam <kathirs1@verifone.com>
2020-08-09 13:41:43 +0530

    add new apis for other queries

--- src/main/java/com/verifone/analytics/spark/transset/SparkTranssetAnalyser.java
+++ src/main/java/com/verifone/analytics/spark/transset/SparkTranssetAnalyser.java
@@ -24,11 +24,14 @@ public class SparkTranssetAnalyser {
 	private static JavaSparkContext jsc = null;
 	private static boolean initializeWriteConfig = false;
 	private static Map<String, String> writeConfigOverrides = null; 
+	private static String siteId = "txndb";
 
 	public static void main(String[] args) {
-
+		// Get site Id from the arug
+		siteId = args[0];
+		
 		try (SparkSession sparkSession = SparkSession.builder().master("local").appName("TranssetAnalyser")
-				.config("spark.mongodb.input.uri", "mongodb://127.0.0.1/txnDB.transactions2")
+				.config("spark.mongodb.input.uri", "mongodb://127.0.0.1/txnDB."+siteId) // e.g., txnDB.site1
 				.config("spark.mongodb.output.uri", "mongodb://127.0.0.1/txnDB.plus").getOrCreate();) {
 			
 			jsc = new JavaSparkContext(sparkSession.sparkContext());
@@ -38,11 +41,22 @@ public class SparkTranssetAnalyser {
 			transactionDS.show();
 
 			SparkTranssetAnalyser app = new SparkTranssetAnalyser();
-			app.groupPLUByDate(sparkSession, transactionDS);
 			
-			/*
-			 * Add your query api's here.
-			 */
+			app.salesByUPCTop(sparkSession, transactionDS);
+			app.salesByUPCBottom(sparkSession, transactionDS);
+			app.salesByCategoryTop(sparkSession, transactionDS);
+			app.salesByCategoryBottom(sparkSession, transactionDS);
+			app.salesByDepartmentTop(sparkSession, transactionDS);
+			app.salesByDepartmentBottom(sparkSession, transactionDS);
+			app.salesByFuelProduct(sparkSession, transactionDS);
+			app.customerWaitTimeByCashier(sparkSession, transactionDS);
+			app.dailySalesCount(sparkSession, transactionDS);
+			app.averageTransactionAmount(sparkSession, transactionDS);
+			app.salesByMOP(sparkSession, transactionDS);
+			app.salesByCardType(sparkSession, transactionDS);
+			app.salesByEntryMethod(sparkSession, transactionDS);
+			app.discountsByLoyaltyProgram(sparkSession, transactionDS);
+			app.recurringCustomerCount(sparkSession, transactionDS);
 
 		} finally {
 			jsc.close();
@@ -50,7 +64,7 @@ public class SparkTranssetAnalyser {
 		}
 
 	}
-
+	
 	/**
 	 * 
 	 * Transforms the transset data from 'transactions' collections at txnDB and writes the 
@@ -59,19 +73,89 @@ public class SparkTranssetAnalyser {
 	 * @param sparkSession
 	 * @param transactionDS
 	 */
-	private void groupPLUByDate(SparkSession sparkSession, Dataset<Row> transactionDS) {
+	private void salesByUPCTop(SparkSession sparkSession, Dataset<Row> transactionDS) {
 
 		Dataset<Row> trLines = transactionDS.select(substring(col("trans.trHeader.date"), 0, 10).as("trDate"),
 				explode(col("trans.trLines.trLine")).as("trLine"));
 		Dataset<Row> filteredPLUWithDate = trLines.filter(col("trLine.type").equalTo("plu"))
 				.select(col("trDate"), col("trLine.trlDesc").as("trPluDesc")).sort(col("trDate").asc());
-		Dataset<Row> groupByDateAndPlu = filteredPLUWithDate.groupBy("trPluDesc", "trDate").count();
-		groupByDateAndPlu.show();
+		Dataset<Row> salesByUpcDF = filteredPLUWithDate.groupBy("trPluDesc", "trDate").count();
+		salesByUpcDF.show();
 		
-		writeToResultSetDB(groupByDateAndPlu, "groupPluByDate");
+		writeToResultSetDB(salesByUpcDF, "salesByUPC");
 		
 	}
+
+	private void salesByUPCBottom(SparkSession sparkSession, Dataset<Row> transactionDS) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void salesByCategoryTop(SparkSession sparkSession, Dataset<Row> transactionDS) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void salesByCategoryBottom(SparkSession sparkSession, Dataset<Row> transactionDS) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void salesByDepartmentTop(SparkSession sparkSession, Dataset<Row> transactionDS) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void salesByDepartmentBottom(SparkSession sparkSession, Dataset<Row> transactionDS) {
+	// TODO Auto-generated method stub
 	
+	}
+
+	private void salesByFuelProduct(SparkSession sparkSession, Dataset<Row> transactionDS) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void customerWaitTimeByCashier(SparkSession sparkSession, Dataset<Row> transactionDS) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void dailySalesCount(SparkSession sparkSession, Dataset<Row> transactionDS) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void averageTransactionAmount(SparkSession sparkSession, Dataset<Row> transactionDS) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void salesByMOP(SparkSession sparkSession, Dataset<Row> transactionDS) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void salesByCardType(SparkSession sparkSession, Dataset<Row> transactionDS) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void salesByEntryMethod(SparkSession sparkSession, Dataset<Row> transactionDS) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void discountsByLoyaltyProgram(SparkSession sparkSession, Dataset<Row> transactionDS) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void recurringCustomerCount(SparkSession sparkSession, Dataset<Row> transactionDS) {
+	// TODO Auto-generated method stub
+	
+	}
+
 	/**
 	 * Common method to write to resultSetDB.
 	 * 
@@ -81,7 +165,7 @@ public class SparkTranssetAnalyser {
 	private static void writeToResultSetDB(Dataset<Row> df, String collectionName) {
 		if(!initializeWriteConfig) {
 			writeConfigOverrides = new HashMap<String, String>();
-			writeConfigOverrides.put("database", "resultSetDB");
+			writeConfigOverrides.put("database", siteId+"ResultsDB"); // site1ResultsDB
 			writeConfigOverrides.put("writeConcern.w", "majority");
 		}
 		writeConfigOverrides.put("collection", collectionName);
